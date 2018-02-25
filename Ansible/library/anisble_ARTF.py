a7194e2fe96e69a14468b654ea8132b99e67b5b5
Shramik Gawde <shramikgawde@shramiks-mbp.home>
2018-02-25 18:11:50 -0500

    added a lot of things

--- Ansible/library/anisble_ARTF.py
+++ Ansible/library/anisble_ARTF.py
@@ -5,16 +5,17 @@ from ansible.module_utils.basic import AnsibleModule
 from pprint import pprint
 
 def call_artifactory(module, product_name, product_version):
-  if product_name == "Apache":
-      new_product_name = product_name + "-http"
-      module.exit_json(msg="%s" % new_product_name, changed=True)
-  elif product_name == "jboss":
-      new_product_name = product_name + "_EAP"
-      module.exit_json(msg="%s" % new_product_name, changed=True)
-  elif product_name == "tomcat":
-      module.exit_json(msg="%s" % product_name, changed=True)
-
-
+  if (product_name == "Apache" or product_name == "apache"):
+      new_product_name = product_name.lower() + "-http"
+      
+      module.exit_json(msg="Successful execution", dir_product_name="%s" % new_product_name, changed=True)
+  elif (product_name == "JBoss_EAP" or product_name == "jboss" or product_name == "JBoss"):
+      new_product_name = product_name[:5].lower()
+      print(new_product_name)
+      module.exit_json(msg="Successful execution", dir_product_name="%s" % new_product_name, changed=True)
+  elif (product_name == "Tomcat" or product_name == "tomcat"):
+      new_product_name = product_name.lower()
+      module.exit_json(msg="Successful execution", dir_product_name="%s" % new_product_name, changed=True)
 
 def main():
   module = AnsibleModule(
